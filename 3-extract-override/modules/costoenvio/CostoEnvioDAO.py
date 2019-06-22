4a7ea87680358f355e84e9fc86c68a59d89e4da4
tuxito <jean@example.com>
2019-06-22 10:51:50 -0500

    cambio repo

--- 3-extract-override/modules/costoenvio/CostoEnvioDAO.py
+++
@@ -1,9 +0,0 @@
-
-
-class CostoEnvioDAO:
-
-    def actualizar(self, pais, costo):
-        raise Exception("No debes consultar a la DB")
-
-    def obtener(self, pais):
-        raise Exception("No debes consultar a la DB")
